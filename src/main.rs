087365ba809e569040f697227ed2cb3ae30cb76e
Martin Källander <jamymartyn@gmail.com>
2026-06-16 22:54:15 +0200

    testing a random generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::{RngExt, SeedableRng};
+use rand::rngs::StdRng;
+
 fn main() {
+    let seed = 123456;
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    let x: i32 = rng.random_range(1..=10);
+
     println!("Hello World!");
+    println!("Random number is: {}", x);
 }
